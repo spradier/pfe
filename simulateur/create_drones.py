4e31c9ae346e5a2da59e928cec24727fa84929e4
Sebastien pradier <57252280+spradier@users.noreply.github.com>
2022-01-05 23:55:02 +0100

    Update create_drones.py

--- simulateur/create_drones.py
+++ simulateur/create_drones.py
@@ -10,19 +10,45 @@ parser.add_argument("--Coordinates", metavar="C", type=str)
 args = vars(parser.parse_args())
 
 vehicles = {}
+sensors = {}
 N = args["Number of drones"]
 C = [0, 0, 0]
 for i in range(N):
     vehicles["Drone"+str(i)] = {"VehicleType" : "SimpleFlight", 
                                 "X" : C[0], 
                                 "Y" : C[1], 
-                                "Z" : C[2]
+                                "Z" : C[2],
+                                "Cameras": {},
                                 }
+
+
+
+
+cameraDefault = {"CaptureSettings": [
+        {
+            "Width": 640,
+            "Height": 480,
+            "FOV_Degrees": 360
+        }
+    ],
+    "NoiseSettings":[
+        {
+            "Enabled": "true",
+            "HorzDistortionContrib": 1.0,
+            "HorzDistortionStrength": 0.002
+        }
+    ],
+}
 
 final_dict = {"SettingsVersion": 1.2,
             "SimMode": "Multirotor",
-            "Vehicles": vehicles
+            "CameraDirector": {
+                "X": 1, "Y": 1, "Z": 1 
+            },
+            "Vehicles": vehicles,
+            "CameraDefaults": cameraDefault
             }
+
 
 with open('data.json', 'w') as outfile:
     json.dump(final_dict, outfile, indent=4)
